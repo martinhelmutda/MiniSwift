5de67feb6f6f10f2adfa0f2ca6c40da0fc983956
Martin Helmut Dominguez Alvarez <martinhelmutmh@gmail.com>
2019-04-15 15:54:20 -0500

    UIKit cambio

--- Juego_de_memoria.playground/Contents.swift
+++ Juego_de_memoria.playground/Contents.swift
@@ -1,11 +1,9 @@
-import Cocoa
-
-
+import UIKit
 for i in 0...100 {
     if i%5 == 0 && i != 0 {print(" #\(i) Bingo!!!")}
-    
+
     if i%2 == 0 && i != 0 {print(" #\(i) par!!!")}
     else if i != 0 {print(" #\(i) impar!!!")}
-    
+
     if 30...40 ~= i {print(" #\(i) Viva Swift!!")}
 }
